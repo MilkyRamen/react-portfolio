a495b33a1d047fe75a0642f90186cba972645f42
MilkyRamen <127574535+MilkyRamen@users.noreply.github.com>
2023-08-14 19:13:58 -0400

    Footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href="https://github.com/MilkyRamen">GitHub</a>
+        </footer>
+    );
+};
+
+export default Footer;
